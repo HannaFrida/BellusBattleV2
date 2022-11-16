62c98e948db120b77c40b3b2739a4a9deb406bd4
HannaFrida <44497532+HannaFrida@users.noreply.github.com>
2022-11-16 15:09:37 +0100

    Transition scene 2.0

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Transition : MonoBehaviour
+{
+    Image image1; // Baloons
+    Image image2; // Baloons
+    GameManager gameManager;
+    int winner;
+
+    private void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    private void Update()
+    {
+        // Use a coroutine to load the Scene in the background
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
+    void MoveUpPlayer()
+    {
+        winner = gameManager.GetWinnerID();
+        
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        StartCoroutine(ExampleCoroutine());
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(gameManager.NextLevel);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+}
