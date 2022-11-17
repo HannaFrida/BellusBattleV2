c91511f1d74a9b9d24ba12520add3852db709031
HannaFrida <44497532+HannaFrida@users.noreply.github.com>
2022-11-17 17:33:56 +0100

    granaten fungerar
    Co-Authored-By: MartinWallmark <70527220+MartinWallmark@users.noreply.github.com>
    Co-Authored-By: Simon <64077342+GreyGeese@users.noreply.github.com>

--- Assets/Scripts/Camera/CameraFocus.cs
+++ Assets/Scripts/Camera/CameraFocus.cs
@@ -48,7 +48,7 @@ public class CameraFocus : MonoBehaviour
 		if (_targets.Contains(t) == false){return false;}
 		
 		_targets.Remove(t);
-        _targetGroup.RemoveMember(t);
+        //_targetGroup.RemoveMember(t);
         return true;
 	}
 }
