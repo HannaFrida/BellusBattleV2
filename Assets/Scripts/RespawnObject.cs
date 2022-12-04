7533382a13bef6e46e3c4d3ea0967ac282414d27
Razerclaws <slutbacktim@gmail.com>
2022-12-04 11:49:24 +0100

    Main

---
+++ Assets/Scripts/RespawnObject.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnObject : MonoBehaviour
+{
+    private bool respawn = false;
+    private float respawnTimer;
+    [SerializeField] private float respawWait = 5f;
+    [SerializeField] private GameObject respawnObject;
+    GameObject SpawnObj;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (respawn)
+        {
+            respawnTimer += Time.deltaTime;
+            //Debug.Log("droppper: " + dropTimer + " poda " + timeToWaitForPickup);
+            if (respawnTimer >= respawWait)
+            {
+                respawn = false;
+                SpawnObj = Instantiate(respawnObject, transform.position, transform.rotation);
+            }
+        }
+        if (SpawnObj == null)
+        {
+            respawn = true;
+        }
+    }
+
+    public void StartRespawn()
+    {
+        respawn = true;
+    }
+}
