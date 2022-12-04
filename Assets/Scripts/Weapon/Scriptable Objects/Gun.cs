654374e759a5b1f2f571f8adc7526248e5b7cdd1
MartinWallmark <70527220+MartinWallmark@users.noreply.github.com>
2022-12-04 20:21:11 +0100

    småfix

--- Assets/Scripts/Weapon/Scriptable Objects/Gun.cs
+++ Assets/Scripts/Weapon/Scriptable Objects/Gun.cs
@@ -155,7 +155,7 @@ public class Gun : MonoBehaviour
             Despawn();
         }
 
-        // SPECIAL CASES
+        // SPECIAL CASES stop pls
 
         if (gunsAmmo == 0 && weaponData.name == "Grenade")
         {
@@ -269,7 +269,7 @@ public class Gun : MonoBehaviour
         gameObject.GetComponent<Gun>().enabled = false;
 
        
-        if (gameObject.GetComponent<MeshFilter>().mesh != null)
+        if (gameObject.GetComponent<MeshFilter>() != null)
         {
             Debug.Log("borde inte vara här");
             Mesh mesh = GetComponent<MeshFilter>().mesh;
