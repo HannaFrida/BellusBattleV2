29eafa9bc7381f26726ec4c45540e5305637dc0f
khalosh74 <alrassk74@gmail.com>
2022-12-17 23:37:00 +0100

    Costomization

--- Assets/Customization.cs
+++ Assets/Customization.cs
@@ -1,51 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class Customization : MonoBehaviour
 {
     [SerializeField] private List<GameObject> hats = new List<GameObject>();
-    private List<GameObject> currenthats;
+    private List<GameObject> availableHats;
+    private List<GameObject> removedHats = new List<GameObject>();
     int random;
     private void Start()
     {
-        currenthats = new List<GameObject>(hats);
+        availableHats = new List<GameObject>(hats);
     }
 
     // Start is called before the first frame update
     [System.Obsolete]
     private void OnTriggerEnter(Collider other)
     {
-        
-        if (!other.TryGetComponent<BoxCollider>(out BoxCollider col))
-            return;
-        print(other);
         if (other.gameObject.CompareTag("Player"))
         {
-            print("1");
-
             GameObject place = other.gameObject.GetComponentInChildren<CharacterCustimization>().transform.FindChild("Headslot").gameObject;
-            if (place.transform.childCount != 0)
-            {
-                
-                //currenthats.Add(hats[random]); 
-                //Destroy(place.transform.GetChild(place.transform.childCount).gameObject);
-            }
-            print("3");
-            random = Random.Range(0, currenthats.Count);
-            GameObject hat = Instantiate(currenthats[random], place.transform);
-            if (place != null)
-            {
-                print("4");
-                hat.transform.parent = place.transform;
-                currenthats.Remove(currenthats[random]);
-            }
-            else Debug.LogError("sök vägen är fel");
-            print("5");
-            // solution 2
+            EntryProtocol(place);
+            PutHat(place);
             // object.transform.GetChild(Random.Range(0, hats.Count));
             //change something if needed
         }
+    }
+
+    private void EntryProtocol(GameObject place)
+    {
+        //check if player have hat
+        //if so remove the hat else continue
+        place = place.transform.GetChild(place.transform.childCount - 1).gameObject;
+        foreach (GameObject hat in removedHats)
+        {
+            if (place == hat)
+            {
+                availableHats.Add(hat);
+                removedHats.Remove(hat);
+                Destroy(place);
+                return;
+            }
+        }
+    }
+    private void PutHat(GameObject place)
+    {
+        random = Random.Range(0, availableHats.Count - 1);
+        GameObject hat = Instantiate(availableHats[random], place.transform);
+        hat.transform.parent = place.transform;
+        removedHats.Add(hat);
+        availableHats.Remove(availableHats[random]);
     }
     //check boxes ...
     //ears
