bbae3e816c456efc882d1304489e2ff06b82df88
khalosh74 <alrassk74@gmail.com>
2022-12-18 13:19:03 +0100

    Costumization v2

--- Assets/Customization.cs
+++ Assets/Customization.cs
@@ -7,13 +7,21 @@ using static UnityEngine.EventSystems.EventTrigger;
 
 public class Customization : MonoBehaviour
 {
+    enum HatList {FromList, FromScene};
+    [SerializeField] private HatList hatList;
     [SerializeField] private List<GameObject> hats = new List<GameObject>();
     private List<GameObject> availableHats;
     private List<GameObject> removedHats = new List<GameObject>();
     int random;
+    GameObject place;
     private void Start()
     {
         availableHats = new List<GameObject>(hats);
+    }
+    private GameObject ChooseHatList()
+    {
+        if(hatList == HatList.FromList) return Instantiate(availableHats[random], place.transform);
+        return availableHats[random];
     }
 
     // Start is called before the first frame update
@@ -22,34 +30,35 @@ public class Customization : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            GameObject place = other.gameObject.GetComponentInChildren<CharacterCustimization>().transform.FindChild("Headslot").gameObject;
+            place = other.gameObject.GetComponentInChildren<CharacterCustimization>().transform.FindChild("Headslot").gameObject;
             EntryProtocol(place);
-            PutHat(place);
+            PutHat();
             // object.transform.GetChild(Random.Range(0, hats.Count));
             //change something if needed
         }
     }
 
-    private void EntryProtocol(GameObject place)
+    private void EntryProtocol(GameObject placeObject)
     {
         //check if player have hat
         //if so remove the hat else continue
-        place = place.transform.GetChild(place.transform.childCount - 1).gameObject;
+        placeObject = place.transform.GetChild(place.transform.childCount - 1).gameObject;
         foreach (GameObject hat in removedHats)
         {
-            if (place == hat)
+            if (placeObject == hat)
             {
                 availableHats.Add(hat);
                 removedHats.Remove(hat);
-                Destroy(place);
+                Destroy(placeObject);
                 return;
             }
         }
     }
-    private void PutHat(GameObject place)
+    private void PutHat()
     {
         random = Random.Range(0, availableHats.Count - 1);
-        GameObject hat = Instantiate(availableHats[random], place.transform);
+        GameObject hat = ChooseHatList();
+        hat.transform.position = place.transform.position + new Vector3(0,1f,0);
         hat.transform.parent = place.transform;
         removedHats.Add(hat);
         availableHats.Remove(availableHats[random]);
