54946d94a22bfb507bfa008eaeca986746e3e7ce
MartinWallmark <70527220+MartinWallmark@users.noreply.github.com>
2023-01-02 14:33:11 +0100

    fix med pausmenyn!

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -69,6 +69,7 @@ public class PauseMenu : MonoBehaviour
     public void ResetSelectedButton()
     {
         isQuitCanvasToggled = false;
+        isLobbyCanvasToggled = false;
         es.SetSelectedGameObject(firstSelected.gameObject);
     }
 
