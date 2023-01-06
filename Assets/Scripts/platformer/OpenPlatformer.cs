e2f9d49e0f66a648862fb91a983f916d2f204bc5
Simon <simon.h.oscarson@hotmail.se>
2023-01-06 13:04:47 +0100

    urt

--- Assets/Scripts/platformer/OpenPlatformer.cs
+++ Assets/Scripts/platformer/OpenPlatformer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Experimental.GraphView.GraphView;
+//using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class OpenPlatformer : MonoBehaviour
 {
